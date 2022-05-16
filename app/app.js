2b8671ccbf2ad967f0f5e594ac5f66e895748c80
zanetteandrea <andrea.zanette@studenti.unitn.it>
2022-05-16 23:28:35 +0200

    Setup branch develop

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+/**
+ * Configure Express.js parsing middleware
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+module.exports = app;
