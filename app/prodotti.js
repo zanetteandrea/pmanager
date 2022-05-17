d9d35710df3be89680d59c4df94bc6a6eb47103b
David <david.petrovic@studenti.unitn.it>
2022-05-17 18:45:55 +0200

    creazione API per cancellazione prodotto

--- app/prodotti.js
+++ app/prodotti.js
@@ -21,12 +21,38 @@ var validator = require('validator');
  *     responses:
  *       201:
  *         description: Created
+ *       400:
+ *         description: Dati inseriti non validi
+ * /prodotti/{id}:
+ *   parameters:
+ *      in: path
+ *      description: id del prodotto da eliminare
+ *      name: id
+ *      required: true
+ *      schema:
+ *          type: integer
+ *   delete:
+ *     summary: Elimina un prodotto
+ *     responses:
+ *       204:
+ *         description: Prodotto eliminato
+ *       404:
+ *         description: Prodotto non trovato
 */
 
 router.post('', (req, res) => {
 
     const { nome, ingredienti, prezzo, foto } = req.body
-
+    if(validator.isEmpty(nome)){
+        console.log('Nome del prodotto non valido');
+        res.status(400).send();
+        return;
+    }
+    if(validator.isEmpty(foto)){
+        console.log('Path foto del prodotto non valida');
+        res.status(400).send();
+        return;
+    }
     let prodotto = new Prodotto({
         nome,
         ingredienti,
@@ -37,8 +63,22 @@ router.post('', (req, res) => {
     prodotto = prodotto.save().then(() => {
         console.log('Prodotto salvato con successo');
     })
-
+    
     res.status(201).send();
+});
+
+router.delete('/:id', async (req, res) => {
+    let prodotto = await Prodotto.findById(req.params.id).exec()
+    console.log('Prodotto trovato');
+    if (!prodotto) {
+        res.status(404).send()
+        console.log("Prodotto non trovato")
+        return;
+    }
+    await prodotto.deleteOne()
+
+    console.log("Prodotto rimosso")
+    res.status(204).send()
 });
 
 module.exports = router;
