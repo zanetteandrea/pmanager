e678aa9062aec2db673c7f7d90d18dcacf0cc4a2
David <david.petrovic@studenti.unitn.it>
2022-05-17 10:50:36 +0200

    config swagger

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const app = require('./app/app.js');
 const mongoose = require('mongoose');
+swaggerJsdoc = require("swagger-jsdoc");
+swaggerUi = require("swagger-ui-express");
 require('dotenv').config()
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "LogRocket Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "LogRocket",
+                url: "https://logrocket.com",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:" + process.env.PORT || 8080,
+            },
+        ],
+    },
+    apis: [],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
 
 const port = process.env.PORT || 8080;
 
