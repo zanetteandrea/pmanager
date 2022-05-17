e47129f4e437f7c032054d66f75f00ea2ccfdb09
David <david.petrovic@studenti.unitn.it>
2022-05-17 11:16:56 +0200

    Aggiunta documentazione prodotto

--- app/prodotti.js
+++ app/prodotti.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const Prodotto = require('./models/prodotto'); // get our mongoose model
+/**
+ * @swagger
+ * /prodotti:
+ *   post:
+ *     summary: Crea un nuovo prodotto
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     nome:
+ *                       type: string
+ *                       description: Il nome del prodotto
+ *                       example: Mantovana
+*/
 
 router.post('', (req, res) => {
 
--- index.js
+++ index.js
@@ -8,18 +8,17 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "LogRocket Express API with Swagger",
-            version: "0.1.0",
+            title: "PManager API",
+            version: "1.0.0",
             description:
-                "This is a simple CRUD API application made with Express and documented with Swagger",
+                "Operazioni CRUD del sistema PManager",
             license: {
                 name: "MIT",
                 url: "https://spdx.org/licenses/MIT.html",
             },
             contact: {
-                name: "LogRocket",
-                url: "https://logrocket.com",
-                email: "info@email.com",
+                name: "Andrea Zanette, David Petrovic, Giovanni De Carlo, Marco Bonafini",
+                url: "https://github.com/zanetteandrea/pmanager",
             },
         },
         servers: [
@@ -28,7 +27,7 @@ const options = {
             },
         ],
     },
-    apis: [],
+    apis: ["app/*.js"],
 };
 
 const specs = swaggerJsdoc(options);
