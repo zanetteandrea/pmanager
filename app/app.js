e2842552a3b3de9bd68059a61addbe3095f6e7be
zanetteandrea <andrea.zanette@studenti.unitn.it>
2022-05-19 22:55:05 +0200

    Fix minor bugs auth

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const auth = require("./auth.js");
-const ruoli = require('./models/ruoli.js');
+const cors = require("cors")
 
 /**
  * Configure Express.js parsing middleware
  */
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(auth.router)
