ce82ae28df2469a69ffb5eb4e559108a3df7a0b6
Giovanni  De Carlo <giovannidecarlo@MacBook-Air-di-Giovanni.local>
2022-05-20 09:09:19 +0200

    Creazione schema dipendente

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+const dipendente = require('./dipendente.js')
+
 /**
  * Configure Express.js parsing middleware
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api/v1/dipendente', dipendente);
 module.exports = app;
